75073c7f855f701d4d8d5ea0ac501f61e59caaa3
Carlos Álvarez <Carlos lvarez>
2017-05-07 17:55:18 +0200

    Heroku

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.model):
+    title=models.CharField(max_length = 150)
+    body=models.TextField()
+    time=models.DateTimeField()
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
